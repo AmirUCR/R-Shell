2f7af0e3b319e9e725a392783ef2310530367c13
AmirUCR <amohs002@ucr.edu>
2019-05-08 17:45:17 -0700

    Add comments

--- header/parser.hpp
+++ header/parser.hpp
@@ -6,9 +6,13 @@ using namespace std;
 
 class Parser {
 	private:
-	string input;
+	string input;	// Holds user's command line input
 	vector<string> commands;
+	
 	public:
-	Parser();
-	void Parse();
+	Parser();		// Get user input and store it
+
+	void Parse();	// Takes the user input and tokenizes it by "&& || ;" delimiters
+					// Instantiates executables as needed
+					// instantiates connectors as needed
 };
