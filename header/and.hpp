545cbf71eb2c315f45127707fc8ac051d10f0406
AmirUCR <amohs002@ucr.edu>
2019-05-09 10:38:53 -0700

    Add initial header file

---
+++ header/and.hpp
@@ -0,0 +1,14 @@
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
+#include "connector.hpp"
+
+using namespace std;
+
+class And : public Connector {
+    public:
+    And(Command*, Command*);    // Initialize
+    bool execute();             // Only return true if left child
+                                // and right child execute both return true
+};
+
+#endif  // END OF CONNECTOR_H
