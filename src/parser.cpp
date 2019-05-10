a813ee8b116ce92f97c0378dbf8be800663460e5
AmirUCR <amohs002@ucr.edu>
2019-05-10 15:08:17 -0700

    Add tokenize functions

---
+++ src/parser.cpp
@@ -0,0 +1,84 @@
+#include "..\header\parser.hpp"
+
+Parser::Parser() {
+	cout << "$ ";
+	//getline(cin, input);
+	this->Parse();
+}
+
+void Parser::Parse() {
+	vector<string> tokenized;
+
+	//input = "     echo hi && echo \"echo && hi\" && echo \" substr sucks  \"#32i09i209ueq        ";
+	input = "   echo hi && echo bye && echo why; \" wadgawdyag echo Tie #u8qiydiqygwdatwldtg         ";
+
+	// We erase anything after # -- Don't want to parse comments
+	size_t findComment = input.find('#');
+	if (findComment != string::npos) {
+		input.erase(findComment);
+	}
+
+	// Trim start and trailing whitespace
+	size_t firstChar = input.find_first_not_of(' ');
+	size_t lastChar = input.find_last_not_of(' ');
+	size_t inputRange = firstChar - lastChar + 1;
+	input = input.substr(firstChar, inputRange);
+
+	unsigned current = 0;
+	unsigned trail = 0;
+
+	// Begin tokenizing
+	while (current < input.length()) {
+
+		// When we find a quotation mark...
+		if (input[current] == '"') {
+			trail = current + 1;
+			current++;
+
+			// ...keep looking for its second pair
+			while (current + 1 < input.length() && input[current] != '"') {
+				current++;
+			}
+
+			// When second pair found, or when we reach the end of string:
+			tokenized.push_back(input.substr(trail, current - trail));
+
+			// If there are more characters after the quote, jump over the space. Move trail
+			if (input[current + 1] == ' ') {
+				current++;
+				trail = current + 1;
+			} else {
+				trail = current + 1;
+			}
+
+		}
+
+		else if (current + 1 >= input.length()) {
+			tokenized.push_back(input.substr(trail));
+			break;		// We've reached the end of input
+		}
+		else if (input[current] == ' ') {
+			tokenized.push_back(input.substr(trail, current - trail));
+			trail = current + 1;
+		}
+		else if (input[current] == ';') {
+			if (trail < current) {
+				tokenized.push_back(input.substr(trail, current - trail));
+				trail = current;
+			} else {
+				tokenized.push_back(input.substr(trail, trail + 1));
+				current++;
+				trail = current + 1;
+			}
+		}
+
+		current++;		// We are at a character
+	}
+
+	
+
+
+	for (auto &str : tokenized) {
+		cout << str << endl;
+	}
+}
