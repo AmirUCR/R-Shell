2dcd5f9acd3132ee7d2d8ab76cfc1daea03775bc
AmirUCR <amohs002@ucr.edu>
2019-05-15 19:13:42 -0700

    Revert

--- header/parser.hpp
+++ header/parser.hpp
@@ -16,17 +16,18 @@ using namespace std;
 class Parser {
 	private:
 	string input{};	// Holds user's command line input
-	//stack<Command*> commands{};
 
 	Connector* WhichConnector(string &s);
 	bool isOperator(string &s);
 	void MakeTree(vector<string> &);
-	void Parse();	// Takes the user input and tokenizes it by "&& || ;" delimiters
-					// Instantiates executables as needed
-					// instantiates connectors as needed
 
 	public:
-	Parser();		// Get user input and store it	
+	Parser(){};
+	void getInput();
+	void parse();	// Takes the user input and tokenizes it by "&& || ;" delimiters
+				// Instantiates executables as needed
+				// instantiates connectors as needed
+
 };
 
 #endif // END PARSER_H
