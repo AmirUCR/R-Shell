9e9db3a66fc43b03ed16eacc5f0bb21f03eed208
AmirUCR <amohs002@ucr.edu>
2019-05-21 14:44:12 -0700

    Multiple changes
    * Include queue to use with MakeTree
    * Add new function: ShuntingYard. Transform input from infix to postfix
    * Add/Remove comments

--- header/parser.hpp
+++ header/parser.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <queue>
 #include "command.hpp"
 #include "executable.h"
 #include "or.hpp"
@@ -17,18 +18,16 @@ class Parser {
 	private:
 	string input{};	// Holds user's command line input
 
-	Connector* WhichConnector(string &s);
-	bool isOperator(string &s);
-	void MakeTree(vector<string> &);
+	Connector* WhichConnector(string &);	// Return a connector object depending on which "&&" "||" or ";" string is passed in
+	bool isOperator(string &);	// True if argument is one of the "&&" "||" or ";"
+	void ShuntingYard(vector<string> &);
+	void MakeTree(queue<string> &);
 
 	public:
 	Parser(){};
-	void getInput();
-	bool parenthesesMatch();
-	void parse();	// Takes the user input and tokenizes it by "&& || ;" delimiters
-				// Instantiates executables as needed
-				// instantiates connectors as needed
-
+	void getInput();			// getline user input
+	bool parenthesesMatch();	// returns true if there is a same number of left "(" and right ")" parentheses in input
+	void parse();				// Takes the user input and tokenizes it by "&& || ;" delimiters
 };
 
 #endif // END PARSER_H
