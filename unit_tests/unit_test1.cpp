54cdf5005898f011eb60b283bae1c1577f0f82e8
AmirUCR <amohs002@ucr.edu>
2019-05-25 17:07:14 -0700

    Add test_cmd tests, add comments

--- unit_tests/unit_test1.cpp
+++ unit_tests/unit_test1.cpp
@@ -1,6 +1,80 @@
 #include "../src/executable.cpp"
 #include "gtest/gtest.h"
 
+#pragma region test_cmd
+//------------------------------------------------START OF TEST_CMD TESTS-----------------------------------------------
+
+TEST(TestClass, noFlagFile){
+    char * args[] = {"test", "unit_tests/unit_test1.cpp", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true); 
+    
+}
+
+
+TEST(TestClass, eFlagFile){
+    char * args[] = {"test", "-e", "unit_tests/unit_test1.cpp", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true); 
+
+}
+
+
+TEST(TestClass, eFlagblah){
+    char * args[] = {"test", "-e", "yooooo", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), false); 
+
+}
+
+TEST(TestClass, fFlagFile){
+    char * args[] = {"test", "-f", "unit_tests/unit_test1.cpp", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true);
+    
+}
+
+TEST(TestClass, dFlagFile){
+    char * args[] = {"test", "-d", "unit_tests/unit_test1.cpp", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), false); 
+}
+
+
+TEST(TestClass, noFlagDirectory){
+    char * args[] = {"test", "unit_tests", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true); 
+}
+
+
+TEST(TestClass, eFlagDirectory){
+    char * args[] = {"test", "-e", "unit_tests", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true); 
+
+}
+
+TEST(TestClass, fFlagDirectory){
+    char * args[] = {"test", "-f", "~/cs100/assns/spring-2019-assignment-cs100-dance-team/unit_tests", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), false); 
+    
+}
+
+TEST(TestClass, dFlagDirectory){
+    char * args[] = {"test", "-d", "unit_tests", NULL}; 
+    Executable* t = new Executable("test", args); 
+    EXPECT_EQ(t->execute(), true); 
+    
+}
+
+#pragma endregion
+//--------------------------------------------------END OF TEST_CMD TESTS--------------------------------------------
+//
+//------------------------------------------------START OF EXECVP TESTS-----------------------------------------------
+#pragma region execvp
+
 
 TEST(ExecutableTest, ValidCommandAndArg){
     
@@ -49,16 +123,15 @@ TEST(ExecutableTest, emptyCommandAndValidArg){
     EXPECT_EQ(exec->execute(), false);
 }
 
-TEST(ExecutableTest, validCommandAndEmptyArg){
-
-    char * argList[] = {NULL};
-
-    Executable * exec = new Executable("ls", argList);
-
-    EXPECT_EQ(exec->execute(), false);
-}
+#pragma endregion
+//------------------------------------------------END OF EXECVP TESTS-----------------------------------------------
+//
+//------------------------------------------------MAIN DRIVER PROGRAM-----------------------------------------------
+#pragma region main
 
 int main(int argc, char **argv) { 
      ::testing::InitGoogleTest(&argc, argv);
      return RUN_ALL_TESTS(); 
 }
+#pragma endregion
+//------------------------------------------------END OF ALL TESTS---------------------------------------------------
