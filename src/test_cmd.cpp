7396295e24ae24b76d0ae21e09d3d4b6acf9c23c
AmirUCR <amohs002@ucr.edu>
2019-05-25 17:22:01 -0700

    Fix cstring comparison

--- src/test_cmd.cpp
+++ src/test_cmd.cpp
@@ -1,18 +1,19 @@
 #include "../header/test_cmd.hpp"
+#include <cstring>
 #include <iostream>
 using namespace std; 
 
 bool Test_cmd::execute() {
     bool status; 
-    struct stat buf; 
-
+    struct stat buf;
+    
     //if a flag is passed in 
-    if (argList[1] == "-e" || argList[1] == "-f" || argList[1] == "-d") {
+    if (strcmp(this->argList[1], "-e") == 0 || strcmp(this->argList[1], "-f") == 0 || strcmp(this->argList[1], "-d") == 0) {
         if (stat(argList[2], &buf) == 0) {
-            if (argList[1] == "-f"){
+            if (strcmp(this->argList[1], "-f") == 0) {
                 status = S_ISREG(buf.st_mode); 
             }
-            if(argList[1] == "-d") {
+            if(strcmp(this->argList[1], "-d") == 0) {
                 status = S_ISDIR(buf.st_mode); 
             }
             else {
