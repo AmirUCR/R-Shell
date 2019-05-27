9c4dfc7426c3854131af73d80b73e98e8edc1d56
AmirUCR <amohs002@ucr.edu>
2019-05-26 20:10:27 -0700

    Add code to delete new objects

--- src/executable.cpp
+++ src/executable.cpp
@@ -7,13 +7,24 @@
 using namespace std;
 
 bool Executable::execute() {
+    bool successFail = false;
 
     // if execName == test, instantiate test -- return test.execute
     if (strcmp(this->execName, "test") == 0) {
         Test_cmd* t = new Test_cmd(execName, argList);
-        return t->execute();
+        successFail = t->execute();
+    
+        delete t;
+        t = 0;
+
+        return successFail;
     } else {
         Execvp* e = new Execvp(execName, argList);
-        return e->execute();
+        successFail = e->execute();
+
+        delete e;
+        e = 0;
+
+        return successFail;
     }
 }
