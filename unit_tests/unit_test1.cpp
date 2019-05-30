79880e9422f91ca64db4518538c309ff320a0e53
AmirUCR <amohs002@ucr.edu>
2019-05-29 18:14:52 -0700

    Tests now pass argList's size to Executable

--- unit_tests/unit_test1.cpp
+++ unit_tests/unit_test1.cpp
@@ -6,7 +6,7 @@
 
 TEST(TestClass, noFlagFile){
     char * args[] = {"test", "unit_tests/unit_test1.cpp", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 3); 
     EXPECT_EQ(t->execute(), true); 
     
 }
@@ -14,7 +14,7 @@ TEST(TestClass, noFlagFile){
 
 TEST(TestClass, eFlagFile){
     char * args[] = {"test", "-e", "unit_tests/unit_test1.cpp", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), true); 
 
 }
@@ -22,55 +22,55 @@ TEST(TestClass, eFlagFile){
 
 TEST(TestClass, eFlagblah){
     char * args[] = {"test", "-e", "yooooo", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 3); 
     EXPECT_EQ(t->execute(), false); 
 
 }
 
 TEST(TestClass, fFlagFile){
     char * args[] = {"test", "-f", "unit_tests/unit_test1.cpp", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), true);
     
 }
 
 TEST(TestClass, dFlagFile){
     char * args[] = {"test", "-d", "unit_tests/unit_test1.cpp", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), false); 
 }
 
 TEST(TestClass, dFlagFolder){
     char * args[] = {"test", "-d", "header/", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), true); 
 }
 
 
 TEST(TestClass, noFlagDirectory){
     char * args[] = {"test", "unit_tests", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 3); 
     EXPECT_EQ(t->execute(), true); 
 }
 
 
 TEST(TestClass, eFlagDirectory){
     char * args[] = {"test", "-e", "unit_tests", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), true); 
 
 }
 
 TEST(TestClass, fFlagDirectory){
     char * args[] = {"test", "-f", "~/cs100/assns/spring-2019-assignment-cs100-dance-team/unit_tests", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), false); 
     
 }
 
 TEST(TestClass, dFlagDirectory){
     char * args[] = {"test", "-d", "unit_tests", NULL}; 
-    Executable* t = new Executable("test", args); 
+    Executable* t = new Executable("test", args, 4); 
     EXPECT_EQ(t->execute(), true); 
     
 }
@@ -86,7 +86,7 @@ TEST(ExecutableTest, ValidCommandAndArg){
     
     char * argList[] = {"ls", "-a", NULL}; 
 
-    Executable * exec = new Executable("ls", argList); 
+    Executable * exec = new Executable("ls", argList, 3); 
 
     EXPECT_EQ(exec->execute(), true); 
 }
@@ -95,7 +95,7 @@ TEST(ExecutableTest, invalidCommandAndArg){
 
     char * argList[] = {"dddd", "dddd", NULL};
 
-    Executable * exec = new Executable("dddd", argList);
+    Executable * exec = new Executable("dddd", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
 }
@@ -105,7 +105,7 @@ TEST(ExecutableTest, invalidCommandValidArg){
 
     char * argList[] = {"ls", "-a", NULL};
 
-    Executable * exec = new Executable("dddd", argList);
+    Executable * exec = new Executable("dddd", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
 }
@@ -115,7 +115,7 @@ TEST(ExecutableTest, validCommandInvalidArg){
 
     char * argList[] = {"dddd", "dddd", NULL};
 
-    Executable * exec = new Executable("ls", argList);
+    Executable * exec = new Executable("ls", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
 }
@@ -124,7 +124,7 @@ TEST(ExecutableTest, emptyCommandAndValidArg){
 
     char * argList[] = {"ls", "-a", NULL};
 
-    Executable * exec = new Executable("", argList);
+    Executable * exec = new Executable("", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
 }
