380b866880430eb974744bc5aa19499dda1732da
AmirUCR <amohs002@ucr.edu>
2019-05-29 18:15:40 -0700

    Check how many args were passed
    * This prevents accessing unallocated memory
    * Prevents seg faults

--- src/test_cmd.cpp
+++ src/test_cmd.cpp
@@ -1,14 +1,26 @@
 #include "../header/test_cmd.hpp"
 #include <cstring>
 #include <iostream>
+#include <algorithm>
 using namespace std; 
 
 bool Test_cmd::execute() {
     bool status; 
     struct stat buf;
+
+    if (argListSize < 3) {
+        cout << "(False)" << endl;
+        return false;
+    }
     
     //if a flag is passed in 
     if (strcmp(this->argList[1], "-e") == 0 || strcmp(this->argList[1], "-f") == 0 || strcmp(this->argList[1], "-d") == 0) {
+        
+        if (argListSize < 4) {
+            cout << "(False)" << endl;
+            return false;
+        }
+
         if (stat(argList[2], &buf) == 0) {
             if (strcmp(this->argList[1], "-f") == 0) {
                 status = S_ISREG(buf.st_mode); 
