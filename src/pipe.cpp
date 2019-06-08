830aba67831983bc3496563f12669ebf49c58456
AmirUCR <amohs002@ucr.edu>
2019-06-07 19:37:08 -0700

    Implement pipe

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -7,8 +7,34 @@
 #include <sys/wait.h>
 #include <iostream>
 
-bool Pipe::execute() {
-// All yours, JJ
+Pipe::Pipe() : Connector() {}
+
+// Piping hot
+bool Pipe::execute(int input_fd, int output_fd) {
+    if (rhsCMD && lhsCMD) {
+        int f_desc[2];
+        int pipe_status = pipe(f_desc);
+
+        if (pipe_status < 0) {
+            std::cerr << "In class Pipe: Failed to create pipe.\n";
+            return false;
+        }
+
+        if (lhsCMD->execute(input_fd, f_desc[1])) {
+            close(f_desc[1]);
+            if (rhsCMD->execute(f_desc[0], output_fd)) {
+                close(f_desc[0]);
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    std::cerr << "In class Pipe: One of left hand side or right hand side is missing.\n";
+    return false;
 }
 
 void Pipe::SetLeft(Command* c) {
@@ -18,3 +44,5 @@ void Pipe::SetLeft(Command* c) {
 void Pipe::SetRight(Command* c) {
     this->rhsCMD = c;
 }
+
+const char * Pipe::getExecName() {}
