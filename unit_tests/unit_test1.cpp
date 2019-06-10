2631cb0a23ed6f3cdef9b1904e515f959f210819
AmirUCR <amohs002@ucr.edu>
2019-06-09 21:59:04 -0700

    Add tests for assignment 4 #33

--- unit_tests/unit_test1.cpp
+++ unit_tests/unit_test1.cpp
@@ -1,4 +1,6 @@
 #include "../src/executable.cpp"
+#include "../src/input_redirector.cpp"
+#include "../src/and.cpp"
 #include "gtest/gtest.h"
 
 #pragma region test_cmd
@@ -89,6 +91,8 @@ TEST(ExecutableTest, ValidCommandAndArg){
     Executable * exec = new Executable("ls", argList, 3); 
 
     EXPECT_EQ(exec->execute(), true); 
+
+    delete exec;
 }
 
 TEST(ExecutableTest, invalidCommandAndArg){
@@ -98,6 +102,8 @@ TEST(ExecutableTest, invalidCommandAndArg){
     Executable * exec = new Executable("dddd", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
+
+    delete exec;
 }
 
 
@@ -108,6 +114,8 @@ TEST(ExecutableTest, invalidCommandValidArg){
     Executable * exec = new Executable("dddd", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
+
+    delete exec;
 }
 
 
@@ -117,7 +125,9 @@ TEST(ExecutableTest, validCommandInvalidArg){
 
     Executable * exec = new Executable("ls", argList, 3);
 
-    EXPECT_EQ(exec->execute(), false);
+    EXPECT_EQ(exec->execute(), true);
+
+    delete exec;
 }
 
 TEST(ExecutableTest, emptyCommandAndValidArg){
@@ -127,11 +137,84 @@ TEST(ExecutableTest, emptyCommandAndValidArg){
     Executable * exec = new Executable("", argList, 3);
 
     EXPECT_EQ(exec->execute(), false);
+
+    delete exec;
 }
 
 #pragma endregion
 //------------------------------------------------END OF EXECVP TESTS-----------------------------------------------
 //
+//------------------------------------------------START OF INPUT REDIRECTION TESTS-----------------------------------------------
+
+// Testing: cat < text.txt
+TEST(Inputredirection_Tests, Valid_Input_File_Test){
+    
+    char * argList1[] = {(char *)"cat", (char *)NULL}; 
+    char * argList2[] = {(char *)"text.txt", (char *)NULL};
+
+    Executable * exec1 = new Executable((char *)"cat", argList1, 2);
+    Executable * exec2 = new Executable((char *)"text.txt", argList2, 2);
+
+    InputRedirector* ir = new InputRedirector();
+    ir->SetLeft(exec1);
+    ir->SetRight(exec2);
+    
+    EXPECT_EQ(ir->execute(0 , 1), true);
+
+    delete ir;
+    delete exec2;
+    delete exec1;
+}
+
+// Testing: cat < invalidFile.txt
+TEST(Inputredirection_Tests, Invalid_Input_File_Test){
+    
+    char * argList1[] = {(char *)"cat", (char *)NULL}; 
+    char * argList2[] = {(char *)"invalidFile.txt", (char *)NULL};
+
+    Executable * exec1 = new Executable((char *)"cat", argList1, 2);
+    Executable * exec2 = new Executable((char *)"invalidFile.txt", argList2, 2);
+
+    InputRedirector* ir = new InputRedirector();
+    ir->SetLeft(exec1);
+    ir->SetRight(exec2);
+    
+    EXPECT_EQ(ir->execute(0 , 1), false);
+
+    delete ir;
+    delete exec2;
+    delete exec1;
+}
+
+TEST(Inputredirection_Tests, Valid_Input_File_Composite_Test){
+    
+    char * argList1[] = {(char *)"cat", (char *)NULL}; 
+    char * argList2[] = {(char *)"text.txt", (char *)NULL};
+
+    Executable* exec1 = new Executable((char *)"cat", argList1, 2);
+    Executable* exec2 = new Executable((char *)"text.txt", argList2, 2);
+
+    InputRedirector* ir = new InputRedirector();
+    ir->SetLeft(exec1);
+    ir->SetRight(exec2);
+
+    char * argList3[] = {(char *)"ls", (char *)NULL};
+
+    Executable* exec3 = new Executable((char *)"ls", argList3, 2);
+
+    And* a = new And();
+    a->SetLeft(ir);
+    a->SetRight(exec3);
+    
+    EXPECT_EQ(a->execute(0 , 1), true);
+
+    delete a;
+    delete ir;
+    delete exec2;
+    delete exec3;
+    delete exec1;
+}
+
 //------------------------------------------------MAIN DRIVER PROGRAM-----------------------------------------------
 #pragma region main
 
